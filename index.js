7693e1307270b096d858d65ab1c308f968f43bca
natan <natan.devback@gmail.com>
2024-06-27 08:16:26 -0300

    update code

--- index.js
+++
@@ -1,7 +0,0 @@
-const fs = require("node:fs")
-
-const data=fs.readFileSync("./steamGames.json","utf-8")
-const games= JSON.parse(data)
-const item = games.find((item) => item.name === "The Apple");
-
-console.log(item);
--- src/Controllers/FindGameController.js
+++ src/Controllers/FindGameController.js
@@ -1,7 +1,14 @@
- const fs=require("node:fs");
+const fs = require("node:fs");
 
+async function findGamesController() {
+	const inicio = performance.now();
 
-	const data = fs.readFileSync("../../steamGames.json", "utf-8");
+	const data = await fs.readFileSync("./steamGames.json", "utf-8");
 	const games = JSON.parse(data);
-	console.log(games)
+	const item = await games.find((item) => item.name === "The Apple");
 
+	const fim = performance.now();
+	console.log(`A operação levou ${fim - inicio} milissegundos`);
+	return item;
+}
+module.exports = { findGamesController };
